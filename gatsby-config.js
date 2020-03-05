0f79dacdce6e2c093d5adaa1f3752b1427607fe4
Mohammed Muzammil ahamed <muzammil@w3layouts.com>
2020-03-05 11:16:50 +0530

    title and description edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Delog`,
+    title: `Muzammil`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://hgn.netlify.com/`,
     home: {
-      title: `Hi! I'm Delog`,
-      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+      title: `Hi! My name is Muzammil`,
+      description: `I am Creative Web Designer and Front-end Developer having 3 years of Experience. I like to make cool and Creative Designs. My design is always full of refreshing ideas. Feel free to take a look around my portfolio`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
     w3l_dom_key: `5e296a6b24860CF_Domain_verify` 
